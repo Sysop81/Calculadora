ac92443e07cf8a7d509d94d43a758193cc0a0bbe
Sysop81 <jose.rlg27@gmail.com>
2020-04-02 14:02:31 +0200

    rama2

--- Calculadora_Git/src/calculadora_git/Calculadora.java
+++ Calculadora_Git/src/calculadora_git/Calculadora.java
@@ -11,7 +11,7 @@ package calculadora_git;
  * de dos atributos que seran los operadores de cada uno de los metodos de los que consta
  * la clase.
  * Subido a GitHub el 30/03/2020
-   ------------> Este comentario ha sido modificado mediante GitHub <-------- 
+   ------------> rama2 <-------- 
  * @author Jose Ramon Lopez Guillen
  * @version 1.0,  20 de Marzo de 2020
  * @since 20 de Marzo de 2020
